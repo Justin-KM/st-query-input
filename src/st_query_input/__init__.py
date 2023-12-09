ee8261443e704c0e3e8fa3eeb5097d5ea44d05c1
Justin-KM <Justin_zhang@kindermorgan.com>
2023-12-09 10:52:16 -0600

    update1

--- src/st_query_input/__init__.py
+++ src/st_query_input/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Tell streamlit that there is a component called st_query_input,
+# Tell streamlit that there is a component called streamlit_multiline_input,
 # and that the code to display that component is in the "frontend" folder
 frontend_dir = (Path(__file__).parent / "frontend").absolute()
 _component_func = components.declare_component(
@@ -12,25 +12,25 @@ _component_func = components.declare_component(
 )
 
 # Create the python function that will be called
-def st_query_input(
+def st_query_input(value, height=28, cols=120, max_height=200,
     key: Optional[str] = None,
 ):
     """
     Add a descriptive docstring
     """
-    component_value = _component_func(
+    component_value = _component_func(value=value, height=height, cols=cols, max_height=max_height,
         key=key,
     )
 
     return component_value
 
-
 def main():
-    st.write("## Example")
-    value = st_query_input()
 
-    st.write(value)
+    st.set_page_config(page_title="test", layout="wide")
 
+    st.write("Multiline text input")
+    valiue = st_query_input("my default valueX", height=30, cols=60)
+    st.write(valiue)
 
 if __name__ == "__main__":
     main()
--- src/st_query_input/frontend/main.js
+++ src/st_query_input/frontend/main.js
@@ -7,21 +7,76 @@ function sendValue(value) {
   Streamlit.setComponentValue(value)
 }
 
+function autoGrow(textarea) {
+  // textarea.style.height = 'auto';
+  // textarea.style.height = textarea.scrollHeight + 'px';
+  // textarea.style.overflowY = 'hidden';
+
+  textarea.style.height = 'auto';
+  textarea.style.height = Math.max(_ini_frame_height_, Math.min(textarea.scrollHeight, _max_frame_height_)) + 'px';
+  textarea.style.overflowY = 'hidden';
+
+  _frame_height_ = parseInt(textarea.style.height, 10);
+}
+
 /**
  * The component's render function. This will be called immediately after
  * the component is initially loaded, and then again every time the
  * component gets new data from Python.
  */
+var _frame_height_ = 20;
+var _ini_frame_height_ = 20;
+var _max_frame_height_ = 200;
 function onRender(event) {
-  // Only run the render code the first time the component is loaded.
-  if (!window.rendered) {
-    // You most likely want to get the data passed in like this
-    // const {input1, input2, input3} = event.detail.args
+  if(!window.rendered) {
+    const {value, cols, height, max_height} = event.detail.args;
 
-    // You'll most likely want to pass some data back to Python like this
-    // sendValue({output1: "foo", output2: "bar"})
-    window.rendered = true
+    _frame_height_ = height;
+    _ini_frame_height_ = height
+    _max_frame_height_ = max_height;
+
+    const input = document.getElementById("input_box");
+    input.cols = cols;
+    input.style.height = height + 'px';
+    if(value) {
+      input.value = value;
+    }
+
+    const btn_submit = document.getElementById("btn_submit");
+    const btn_reset = document.getElementById("btn_reset");
+
+    btn_submit.onclick = event => sendValue(input.value);
+    btn_reset.onclick = event => {
+      input.value = "";
+      input.style.height = _ini_frame_height_ + 'px';
+      sendValue({"reset": true, "height":_ini_frame_height_});
+    }
+    
+    // input.onkeyup = event => sendValue(event.target.value);
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        sendValue({"submit": input.value, height: input.style.height});
+      } else if (event.key === 'Enter' && event.shiftKey) {
+        event.preventDefault();
+        const start = input.selectionStart;
+        const end = input.selectionEnd;
+        const value = input.value;
+        input.value = value.substring(0, start) + '\n' + value.substring(end);
+        input.selectionStart = input.selectionEnd = start + 1;
+
+        input.style.height = 'auto';
+        input.style.height = input.scrollHeight + 'px';
+
+        sendValue({height: input.scrollHeight})
+      }
+    });
+
+    window.rendered = true;
   }
+
+  console.log("height", _ini_frame_height_, _frame_height_, _max_frame_height_)
+  Streamlit.setFrameHeight(Math.min(_frame_height_, _max_frame_height_)+60)
 }
 
 // Render the component whenever python send a "render event"
@@ -29,4 +84,4 @@ Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
 // Tell Streamlit that the component is ready to receive events
 Streamlit.setComponentReady()
 // Render with the correct height, if this is a fixed-height component
-Streamlit.setFrameHeight(100)
+// Streamlit.setFrameHeight(Math.min(_frame_height_, _max_frame_height_)+45)
